82b2d7f2761f94088d06a3ade8d85fbd1d911036
markov45@mail.ru <markov45@mail.ru>
2016-06-27 01:03:55 +0500

    update

---
+++ reg.h
@@ -0,0 +1,46 @@
+#include <Windows.h>
+#include <string>
+#include <vector>
+
+
+class reg
+{
+private:
+
+	HKEY section;
+
+	void check_end_of_string(std::wstring &str);
+
+	std::vector<std::wstring> wcs_to_multi_sz(wchar_t *str, unsigned int size);
+	wchar_t* multi_sz_to_wcs(std::vector<std::wstring> multi_string, unsigned int &size);
+	
+public:
+
+	HKEY key;
+
+	reg();
+	~reg();
+
+	void set_section(HKEY t_section);
+	HKEY get_section();
+
+	bool create_key(std::wstring path);
+	bool delete_key(std::wstring path);
+
+	bool write_binary_value(std::wstring path, std::wstring name, std::string value);
+	bool write_dword_value(std::wstring path, std::wstring name, long value);
+	bool write_qword_value(std::wstring path, std::wstring name, long long value);
+	bool write_sz_or_expand_value(std::wstring path, std::wstring name, bool expand, std::wstring value);
+	bool write_multi_sz_value(std::wstring path, std::wstring name, std::vector<std::wstring> value);
+
+	bool read_binary_value(std::wstring path, std::wstring name, std::string &value);
+	bool read_dword_value(std::wstring path, std::wstring name, long &value);
+	bool read_qword_value(std::wstring path, std::wstring name, long long &value);
+	bool read_sz_or_expand_value(std::wstring path, std::wstring name, std::wstring &value);
+	bool read_multi_sz_value(std::wstring path, std::wstring name, std::vector<std::wstring> &value);
+
+	bool multi_sz_add_value(std::wstring path, std::wstring name, std::wstring value);
+	bool multi_sz_delete_value(std::wstring path, std::wstring name, std::wstring value);
+
+	bool delete_value(std::wstring path, std::wstring name);
+};
